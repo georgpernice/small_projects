7922b6e614f336b7e4b84ebd1dd8414f9b488cf6
georg <georgpernice@gmail.com>
2024-04-03 13:04:25 +0200

    implemented a tests which may work and created launch config for it

--- PDFmerger/tests/test_pdf_merger.py
+++ PDFmerger/tests/test_pdf_merger.py
@@ -1,9 +1,14 @@
-from pikepdf import Pdf
-import sys
-import os
+from pathlib import Path
 import unittest
+from src.pdfmerger.pdf_merger import PDFMerger # TODO actually this way we dont use the installed version which is bad, but works for now.
+
+RESOURCES_PATH = Path(__file__) / "resources"
 
 class TestPDFMerger(unittest.TestCase):
-    def test_merge(self : "TestPDFMerger"):
-        """This tests PDMMerger.merge() method."""
-        assert False #TODO implement this test!
+    def test_merge_gives_enough_pages(self : "TestPDFMerger"):
+        """This tests PDMMerger.merge() method by asserting that the merged PDF is three pages long when merging three onepage PDFs.""" 
+        expected_numb_of_pages = 3
+        merger = PDFMerger()
+        pdf_list = [RESOURCES_PATH / "".join(["unmerged", str(i), ".pdf"]) for i in range(3)]
+        merged_document = merger.merge(pdf_list)
+        assert merged_document.pages() == expected_numb_of_pages
