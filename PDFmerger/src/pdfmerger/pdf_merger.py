206cf479f2fbf1cccf5c4db2fc3f7d50b3b83a29
georg <georgpernice@gmail.com>
2024-04-03 14:28:26 +0200

    tests passing

--- PDFmerger/src/pdfmerger/pdf_merger.py
+++ PDFmerger/src/pdfmerger/pdf_merger.py
@@ -1,40 +1,53 @@
+"""Definition of PDFMerger class for merging pdfs and handling of drag and drop start of this package hopefully soon."""
 from pikepdf import Pdf
 import sys
 import os
-#os.remove("demofile.txt") 
-
-try: #appending wanted -> append file
-    droppedFile = sys.argv[1]
-    try:        
-        statefile = open("state.txt", "r")
-        statefile.close()
-        statefile = open("state.txt", "a")
-        statefile.write(droppedFile+"\n") # ACHTUNG Enter am File-Ende
-        statefile.close()
-        print("appended")
-    except:# deleted statefile
-        print("Restart without file!")
-        
-except:# ending or restart wanted
-    print("No File given.")
-    try:# restart -> recreate statefile
-        statefile =  open('state.txt','x')
-        statefile.close()
-        print("appending. next time open with file!")
-
-    except:# ending -> merge everything from statefile delete afterwards
-        statefile =  open('state.txt','r')
-        pdfs = statefile.readlines()
-        statefile.close()
-        os.remove("state.txt")
-        
+from pathlib import Path
+class PDFMerger:
+    def __init__(self, dest_path : Path) -> None:
+        self.destination_path = dest_path
+    def merge(self, pdfs : list[Path]):
+        """Merge a list of documents into one called merged.pdf."""
         pdf = Pdf.new()
         for file in pdfs:
-            pdf.pages.extend(  [Pdf.open(file).pages[0]]   )
+            src = Pdf.open(file).pages
+            pdf.pages.extend(src) 
+        Path.mkdir(self.destination_path.parent, parents=True, exist_ok=True) 
+        pdf.save(self.destination_path)
+        return self.destination_path
+
+# try: #appending wanted -> append file
+#     droppedFile = sys.argv[1]
+#     try:        
+#         statefile = open("state.txt", "r")
+#         statefile.close()
+#         statefile = open("state.txt", "a")
+#         statefile.write(droppedFile+"\n") # ACHTUNG Enter am File-Ende
+#         statefile.close()
+#         print("appended")
+#     except:# deleted statefile
+#         print("Restart without file!")
+        
+# except:# ending or restart wanted
+#     print("No File given.")
+#     try:# restart -> recreate statefile
+#         statefile =  open('state.txt','x')
+#         statefile.close()
+#         print("appending. next time open with file!")
+
+#     except:# ending -> merge everything from statefile delete afterwards
+#         statefile =  open('state.txt','r')
+#         pdfs = statefile.readlines()
+#         statefile.close()
+#         os.remove("state.txt")
+        
+#         pdf = Pdf.new()
+#         for file in pdfs:
+#             pdf.pages.extend(  [Pdf.open(file).pages[0]]   )
             
-        pdf.save('merged2.pdf')
-        pdf.close()
-        print("merged. next time open without file for restart!")
+#         pdf.save('merged2.pdf')
+#         pdf.close()
+#         print("merged. next time open without file for restart!")
         
 
 
--- PDFmerger/tests/test_pdf_merger.py
+++ PDFmerger/tests/test_pdf_merger.py
@@ -1,14 +1,26 @@
 from pathlib import Path
+from pikepdf import Pdf
 import unittest
+import shutil
 from src.pdfmerger.pdf_merger import PDFMerger # TODO actually this way we dont use the installed version which is bad, but works for now.
 
-RESOURCES_PATH = Path(__file__) / "resources"
-
+RESOURCES_PATH = Path(__file__).parent / "resources"
+OUTPUT_PATH = Path(__file__).parent / "output"
 class TestPDFMerger(unittest.TestCase):
     def test_merge_gives_enough_pages(self : "TestPDFMerger"):
-        """This tests PDMMerger.merge() method by asserting that the merged PDF is three pages long when merging three onepage PDFs.""" 
+        """This tests PDMMerger.merge() method by asserting that the merged PDF is three pages long when merging three onepage PDFs."""
+        # prepare
         expected_numb_of_pages = 3
-        merger = PDFMerger()
-        pdf_list = [RESOURCES_PATH / "".join(["unmerged", str(i), ".pdf"]) for i in range(3)]
-        merged_document = merger.merge(pdf_list)
-        assert merged_document.pages() == expected_numb_of_pages
+        Path.mkdir(OUTPUT_PATH, parents=True, exist_ok=True)
+        destination = OUTPUT_PATH / "merged.pdf"
+        output_was_empty_before = not [ i for i in OUTPUT_PATH.iterdir()] 
+        merger = PDFMerger(destination)
+        pdf_list = [RESOURCES_PATH / "".join(["unmerged", str(i+1), ".pdf"]) for i in range(3)]
+        # tests and asserts
+        merger.merge(pdf_list)
+        assert pdf_list[0].name == "unmerged1.pdf"
+        assert destination.exists()
+        assert len(Pdf.open(destination).pages) == expected_numb_of_pages
+        # cleanup
+        if output_was_empty_before:
+            shutil.rmtree(OUTPUT_PATH)
