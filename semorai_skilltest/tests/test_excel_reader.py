8c95fefb7c51bf09895e712e280de843fe869694
georg <georgpernice@gmail.com>
2024-04-05 09:11:51 +0200

    delete an import and specify the tested row of the excel in a variable when testing

--- semorai_skilltest/tests/test_excel_reader.py
+++ semorai_skilltest/tests/test_excel_reader.py
@@ -1,7 +1,6 @@
 import unittest
 from pathlib import Path
-from semorai_skilltest import DatasheetCrawler
-from semorai_skilltest import ExcelReader
+from semorai_skilltest.excel_reader import ExcelReader
 
 EXCEL_PATH = Path(__file__).parent / "resources" / "Test_data.xlsx"
 
@@ -12,17 +11,19 @@ class TestExcelReader(unittest.TestCase):
     def test_get_product_number(self: "TestExcelReader"):
         """Test that method reads the product link from excel."""
         # setup
+        line_a1 = 1
         expected_content_a2 = 1
         reader = ExcelReader(EXCEL_PATH)
         # test
         col_a = reader.get_product_number()
-        assert expected_content_a2 == col_a[1]
+        assert expected_content_a2 == col_a[line_a1]
 
     def test_get_product_url(self: "TestExcelReader"):
         """Test that method reads the product link from excel."""
         # setup
+        line_b2 = 1
         expected_content_b2 = "https://www.murata.com/en-global/api/pdfdownloadapi?cate=luCeramicCapacitorsSMD&partno=GRM319R71H224KA01#"
         reader = ExcelReader(EXCEL_PATH)
         # test
         col_b = reader.get_product_url()
-        assert expected_content_b2 == col_b[1]
+        assert expected_content_b2 == col_b[line_b2]
